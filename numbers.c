4ed3b38b388f7203ce7c87a649ca793279fdc0b5
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-30 15:46:52 +0530

    Added shell script for linking files also Added create_list and create_node function.

--- numbers.c
+++ numbers.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+Node_ptr create_node(int value)
+{
+  Node_ptr node = malloc(sizeof(Node));
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
+
+List_ptr create_list(void)
+{
+  List_ptr list = malloc(sizeof(List));
+  list->head = NULL;
+  list->last = NULL;
+  list->count = 0;
+  return list;
+}
+
 int main(void)
 {
+  List_ptr list = create_list();
   return 0;
 }
